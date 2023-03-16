c9cafc1b6552658c8d05da5f128764a15b484165
moritz-steiner <128054784+moritz-steiner@users.noreply.github.com>
2023-03-16 15:58:24 +0000

    Create evaluate.sh

---
+++ scripts/evaluate.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+scripts=`dirname "$0"`
+base=$scripts/..
+
+data=$base/data
+configs=$base/configs
+
+translations=$base/translations
+
+mkdir -p $translations
+
+src=de
+trg=en
+
+# cloned from https://github.com/bricksdont/moses-scripts
+MOSES=$base/tools/moses-scripts/scripts
+
+num_threads=6
+device=5
+
+model_name=transformer_iwslt14_deen_bpe
+
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.raw.$src > $translations/test.full.$model_name.$trg
