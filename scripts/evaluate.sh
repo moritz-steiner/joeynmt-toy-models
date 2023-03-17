9510a1f481c4690df863dff9f6433b1ec0a55d40
moritz-steiner <128054784+moritz-steiner@users.noreply.github.com>
2023-03-17 05:16:25 +0000

    Update evaluate.sh

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -22,12 +22,16 @@ device=5
 model_name=transformer_iwslt14_deen_bpe
 
 ### Translation
-
 CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt translate $configs/$model_name.yaml < $data/test.raw.$src > $translations/test.full.$model_name.$trg
+
+### Postprocessing
+# redo tokenization one way
+cat $translations/test.full.$model_name.$trg | $MOSES/tokenizer/tokenizer.perl -l $trg > $translations/test.retokenized.$model_name.$trg
+
+# TODO: your postprocessing here
 
 ### BLEU Comparison
 # compute case-sensitive BLEU of the fully deprocessed texts with sacrebleu internal tokenization
 cat $translations/test.full.$model_name.$trg | sacrebleu $data/test.raw.$trg
 
-# compute case-sensitive BLEU on differently (re)tokenized data
 # TODO: your evaluations here.
